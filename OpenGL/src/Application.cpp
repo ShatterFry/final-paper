c6cb3c009867457e9e683ffad73cf7a0adab96eb
Nikita Kaverin <nikwhite333@gmail.com>
2018-05-22 11:07:52 +0300

    write some code for plant class and age

--- OpenGL/src/Application.cpp
+++ OpenGL/src/Application.cpp
@@ -1,70 +1,119 @@
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <iostream>
+#include <vector>
+
+#define SCREEN_WIDTH 640
+#define SCREEN_HEIGHT 480
+
+enum Age {p, j, im, v, g1, g2, g3, ss, s};
+
+class Plant {
+private:
+	GLfloat x, y, z, radius;
+	Age age;
+public:
+	Plant();
+	~Plant();
+};
+
+Plant::Plant() {
+
+}
+
+Plant::~Plant() {
+
+}
+
+std::vector<Plant> plants;
+
+void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides);
 
 int main(void)
 {
-	GLFWwindow* window;
+	GLFWwindow *window;
 
-	/* Initialize the library */
+	// Initialize the library
 	if (!glfwInit())
+	{
 		return -1;
+	}
 
-	/* Create a windowed mode window and its OpenGL context */
-	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+	// Create a windowed mode window and its OpenGL context
+	window = glfwCreateWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello World", NULL, NULL);
+
 	if (!window)
 	{
 		glfwTerminate();
 		return -1;
 	}
 
-	/* Make the window's context current */
+	// Make the window's context current
 	glfwMakeContextCurrent(window);
 
-	if (glewInit() != GLEW_OK) {
-		std::cout << "Error!" << std::endl;
-	}
+	glViewport(0.0f, 0.0f, SCREEN_WIDTH, SCREEN_HEIGHT); // specifies the part of the window to which OpenGL will draw (in pixels), convert from normalised to pixels
+	glMatrixMode(GL_PROJECTION); // projection matrix defines the properties of the camera that views the objects in the world coordinate frame. Here you typically set the zoom factor, aspect ratio and the near and far clipping planes
+	glLoadIdentity(); // replace the current matrix with the identity matrix and starts us a fresh because matrix transforms such as glOrpho and glRotate cumulate, basically puts us at (0, 0, 0)
+	glOrtho(0, SCREEN_WIDTH, 0, SCREEN_HEIGHT, 0, 1); // essentially set coordinate system
+	glMatrixMode(GL_MODELVIEW); // (default matrix mode) modelview matrix defines how your objects are transformed (meaning translation, rotation and scaling) in your world
+	glLoadIdentity(); // same as above comment
 
-	std::cout << glGetString(GL_VERSION) << std::endl;
-
-	float positions[6] = {
-		-0.5f, -0.5f,
-		0.0f, 0.5f,
-		0.5f, -0.5f
-	};
-
-	unsigned int buffer;
-	glGenBuffers(1, &buffer);
-	glBindBuffer(GL_ARRAY_BUFFER, buffer);
-	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
-
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
-
-	glBindBuffer(GL_ARRAY_BUFFER, 0);
-
-	/* Loop until the user closes the window */
+					  // Loop until the user closes the window
 	while (!glfwWindowShouldClose(window))
 	{
-		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		// render OpenGL here
+		drawCircle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 0, 12, 10);
+		drawCircle(30, 40, 0, 12, 10);
 
-		/*glBegin(GL_TRIANGLES);
-		glVertex2f(-0.5f, -0.5f);
-		glVertex2f(0.0f, 0.5f);
-		glVertex2f(0.5f, -0.5f);
-		glEnd();*/
-
-		/* Swap front and back buffers */
+		// Swap front and back buffers
 		glfwSwapBuffers(window);
 
-		/* Poll for and process events */
+		// Poll for and process events
 		glfwPollEvents();
 	}
 
 	glfwTerminate();
+
 	return 0;
+}
+
+void drawCircle(GLfloat x, GLfloat y, GLfloat z, GLfloat radius, GLint numberOfSides)
+{
+	int numberOfVertices = numberOfSides + 2;
+
+	GLfloat twicePi = 2.0f * M_PI;
+
+	GLfloat circleVerticesX[12];
+	GLfloat circleVerticesY[12];
+	GLfloat circleVerticesZ[12];
+
+	circleVerticesX[0] = x;
+	circleVerticesY[0] = y;
+	circleVerticesZ[0] = z;
+
+	for (int i = 1; i < numberOfVertices; i++)
+	{
+		circleVerticesX[i] = x + (radius * cos(i *  twicePi / numberOfSides));
+		circleVerticesY[i] = y + (radius * sin(i * twicePi / numberOfSides));
+		circleVerticesZ[i] = z;
+	}
+
+	GLfloat allCircleVertices[(12) * 3];
+
+	for (int i = 0; i < numberOfVertices; i++)
+	{
+		allCircleVertices[i * 3] = circleVerticesX[i];
+		allCircleVertices[(i * 3) + 1] = circleVerticesY[i];
+		allCircleVertices[(i * 3) + 2] = circleVerticesZ[i];
+	}
+
+	glEnableClientState(GL_VERTEX_ARRAY);
+	glVertexPointer(3, GL_FLOAT, 0, allCircleVertices);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, numberOfVertices);
+	glDisableClientState(GL_VERTEX_ARRAY);
 }
